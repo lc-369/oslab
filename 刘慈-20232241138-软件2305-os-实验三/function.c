5da7deb8ea087a69adeb11510b26f4f0d525af99
lc-369 <3636197906@qq.com>
2025-05-20 19:20:36 +0800

    Update function.c

--- 刘慈-20232241138-软件2305-os-实验三/function.c
+++ 刘慈-20232241138-软件2305-os-实验三/function.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 int fx(int x) {
@@ -16,6 +15,12 @@ int fy(int y) {
 }
 
 int main() {
+    int fd[2];
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
     pid_t pid_fx, pid_fy;
     int x, y;
     printf("Input x and y:\n");
@@ -24,21 +29,33 @@ int main() {
     // 创建第一个子进程计算 f(x)
     pid_fx = fork();
     if (pid_fx == 0) {
+        close(fd[0]); // 关闭管道读端
         printf("f(x) = %d\n", fx(x));
+        write(fd[1], &fx(x), sizeof(fx(x))); // 将 f(x) 的结果写入管道
+        close(fd[1]); // 关闭管道写端
         exit(0);
     }
 
     // 创建第二个子进程计算 f(y)
     pid_fy = fork();
     if (pid_fy == 0) {
+        close(fd[0]); // 关闭管道读端
         printf("f(y) = %d\n", fy(y));
+        write(fd[1], &fy(y), sizeof(fy(y))); // 将 f(y) 的结果写入管道
+        close(fd[1]); // 关闭管道写端
         exit(0);
     }
 
+    close(fd[1]); // 父进程关闭管道写端
     wait(NULL);
     wait(NULL);
 
-    printf("f(x, y) = %d\n", fx(x) + fy(y));
+    int result_x, result_y;
+    read(fd[0], &result_x, sizeof(result_x)); // 从管道读取 f(x) 的结果
+    read(fd[0], &result_y, sizeof(result_y)); // 从管道读取 f(y) 的结果
+    printf("f(x, y) = %d\n", result_x + result_y);
+
+    close(fd[0]); // 关闭管道读端
 
     return 0;
 }
